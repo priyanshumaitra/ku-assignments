5c75e7985f0030bc083913e596bb8202bcbb6b9c
priyanshu <priyanshumaitra@gmail.com>
2022-11-19 00:07:28 +0530

    classwork added & minor arrangements

---
+++ IT394_IT-Workshop-Python/classworks/18-11-22.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print("hello"*10)
+
+
+# In[4]:
+
+
+print
+
+
+# In[7]:
+
+
+print("hello world"*10)
+
+
+# In[8]:
+
+
+a = "hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello"
+
+
+# In[9]:
+
+
+a.count("hello")
+
+
+# In[11]:
+
+
+a.startswith("world")
+
+
+# In[12]:
+
+
+a.endswith("hello")
+
+
+# In[13]:
+
+
+a.find("hello")
+
+
+# In[14]:
+
+
+a.find("w")
+
+
+# In[15]:
+
+
+a.index("world")
+
+
+# In[16]:
+
+
+a.find("world")
+
+
+# In[17]:
+
+
+a.find("h")
+
+
+# In[18]:
+
+
+a.alnum("hello2")
+
+
+# In[19]:
+
+
+a.alnum("hello")
+
+
+# In[23]:
+
+
+p = "priyanshu"
+
+
+# In[25]:
+
+
+p.isalnum()
+
+
+# In[26]:
+
+
+p = "priyanshu%^&%"
+
+
+# In[27]:
+
+
+p.isalnum()
+
+
+# In[28]:
+
+
+p.capitalize()
+
+
+# In[29]:
+
+
+n = "hello world"
+
+
+# In[32]:
+
+
+n = n.capitalize()
+
+
+# In[33]:
+
+
+n
+
+
+# In[34]:
+
+
+n = n.title()
+
+
+# In[35]:
+
+
+n
+
+
+# In[36]:
+
+
+n = n.swapcase()
+
+
+# In[37]:
+
+
+n
+
+
+# In[38]:
+
+
+n.upper()
+
+
+# In[39]:
+
+
+n.lower()
+
+
+# In[40]:
+
+
+q = "hello^%&^$%^$%#$$%#!@#$@$%^&&^%$#@#$%^&*&^%$#@!"
+
+
+# In[43]:
+
+
+ord("A") //hw
+
+
+# In[57]:
+
+
+w = "   breaking bad is a good series   "
+
+
+# In[58]:
+
+
+w.strip()
+
+
+# In[59]:
+
+
+w.strip()
+
+
+# In[60]:
+
+
+w.split()
+
+
+# In[61]:
+
+
+w
+
+
+# In[62]:
+
+
+l = ['breaking', 'bad', 'is', 'a', 'good', 'series', 20, 344]
+
+
+# In[67]:
+
+
+e = [23, 45, 22, 89, 90, 5000]
+
+
+# In[69]:
+
+
+max(e)
+
+
+# In[70]:
+
+
+sum(e)
+
+
+# In[71]:
+
+
+min(e)
+
+
+# In[81]:
+
+
+v = [0 for i in range (10000)]
+
+
+# In[75]:
+
+
+l = [34, 35, 77, 88, 55, 78, 22, 10, 5, 9, 5, ]
+
+
+# In[76]:
+
+
+l[0] = 50
+
+
+# In[77]:
+
+
+l
+
+
+# In[78]:
+
+
+l [3] = 555
+
+
+# In[79]:
+
+
+l
+
+
+# In[83]:
+
+
+v = [0 for i in range (1000)]
+
+
+# In[92]:
+
+
+for i in range (0, 1000, 5):
+        print(i)
+
+
+# In[95]:
+
+
+a = [34, 44, 55, 10, 5, 9]
+
+
+# In[105]:
+
+
+sorted(a, reverse = True)
+
+
+# In[108]:
+
+
+t = [[20, 20, 6],[30, 4, 7],[40, 77, 90]]
+
+
+# In[109]:
+
+
+print(t)
+
+
+# In[113]:
+
+
+t[0][2]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
