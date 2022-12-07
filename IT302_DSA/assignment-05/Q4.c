de00fc6a27d1e31e16f32ee705bcb8e7f542606c
priyanshu <priyanshumaitra@gmail.com>
2022-12-07 20:51:57 +0530

    assignment-05 updated

---
+++ IT302_DSA/assignment-05/Q4.c
@@ -0,0 +1,68 @@
+// WAP to evaluate a given prefix expression using stack
+#include <stdio.h>
+#include <string.h>
+
+int s[50];
+int top = 0;
+void push(int ch);
+int pop();
+
+int main()
+{
+    int a, b, c, i;
+    char prefix[50];
+
+    printf("Enter the prefix string in figures: ");
+    gets(prefix);
+
+    for (i = strlen(prefix) - 1; i >= 0; i--)
+    {
+        if (prefix[i] == '+')
+        {
+            c = pop() + pop();
+            push(c);
+        }
+        else if (prefix[i] == '-')
+        {
+            a = pop();
+            b = pop();
+            c = b - a;
+            push(c);
+        }
+        else if (prefix[i] == '*')
+        {
+            a = pop();
+            b = pop();
+            c = b * a;
+            push(c);
+        }
+        else if (prefix[i] == '/')
+        {
+            a = pop();
+            b = pop();
+            c = b / a;
+            push(c);
+        }
+        else
+        {
+            push(prefix[i] - 48);
+        }
+    }
+    printf("\nPrefix Expression: %d", pop());
+
+    return 0;
+}
+
+void push(int ch)
+{
+    top++;
+    s[top] = ch;
+}
+
+int pop()
+{
+    int ch;
+    ch = s[top];
+    top = top - 1;
+    return (ch);
+}
